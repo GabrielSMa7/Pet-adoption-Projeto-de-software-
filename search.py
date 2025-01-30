ce197e06f8d4a272faee54ce3eb96fb6348276ff
Gabriel <131276102+GabrielSMa7@users.noreply.github.com>
2025-01-30 12:21:22 -0300

    Add files via upload
    Implementação de busca

---
+++ search.py
@@ -0,0 +1,8 @@
+def filtr(list, fltrs, sps):
+    
+    newlist = {}
+    for item, item_info in list.items():
+        if sps == item_info[fltrs]:
+          newlist[item] = list[item].copy()
+    print(f"{len(newlist)} pets available!\n")
+    return newlist
